8f20dd154df0023f40b38c00c71cce51931c85f1
Christian Höltje <docwhat@gerf.org>
2001-01-05 03:13:12 +0000

    This commit was manufactured by cvs2svn to create tag 'beta-2-6'.

---
+++ tags/beta-2-6/draw.h
@@ -0,0 +1,73 @@
+/* $Id$
+
+    CWimp - Dice game for the palm handhelds.
+    Copyright (C) 1999-2001 Christian Höltje
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+#ifndef _DRAW_H_
+#define _DRAW_H_
+
+#include "game.h"
+
+
+// Symbol Font3
+#define BlankSymbol "\22" 
+#define CheckSymbol "\20"
+#define CurrSymbol  "<"
+#define OutSymbol   "x"
+#define LeadSymbol  "L"
+// 20 - dot
+// 22 - Empty
+// 23 - diamond
+// 26 - check
+//  3 - left double arrow
+//  4 - right double arrow
+//  7 - down arrow
+
+#define CubesLeft   2  // Start Drawing Cubes from here
+#define CubesTop   19  // From the top, remember that the tab is about 15
+#define CubeShift   2  // Space between Cubes
+#define CubeSize   20  // This size Cube should do
+
+
+void DrawIntro();
+void DrawCurrScore();
+void DrawState();
+
+void DrawCube(Int die);
+void EraseCube(Int die);
+void DrawBlackCube(Int die);
+void DrawWhiteCube(Int die);
+void CrossCube(Int die);
+
+void DrawPlayerScore(Short player);
+void ClearKeepBits(void);
+void DrawKeepBit(Int die);
+void DrawTopStatusButton();
+void DrawRollButton();
+void DrawStayButton();
+
+
+void PlaySound(Int type);
+
+void ShowControl(Word objID, Boolean enable);
+void ShowButtons(Int show);
+#ifdef DEBUG
+void EQStatus(Int x);
+#endif
+
+#endif
---
+++ tags/beta-2-6/lowlevel.c
@@ -0,0 +1,111 @@
+/* $Id$ 
+
+    CWimp - Dice game for the palm handhelds.
+    Copyright (C) 1999-2001 Christian Höltje
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+#include "lowlevel.h"
+
+
+/* DrawBitmap -- Places a bitmap at a specified location
+ * Args:    
+ *     Word  BitmapID  -- ID for bitmap, see the cwimp.rpc file
+ *     int   formX     -- X location on the form
+ *     int   formY     -- Y location on the form
+ * Returns: None
+ */
+void DrawBitmap (Word BitmapID, int formX, int formY)
+{
+  VoidHand resourceHandle;
+  BitmapPtr bitmapPntr;
+
+  resourceHandle = DmGetResource(bitmapRsc, BitmapID);
+  bitmapPntr = MemHandleLock(resourceHandle);
+  WinDrawBitmap(bitmapPntr, formX, formY);
+  MemPtrUnlock(bitmapPntr);
+  DmReleaseResource(resourceHandle);
+}
+
+	
+/* SetFieldTextFromStr -- Fills a field with a string
+ * Args:    
+ *     Word     fieldID  -- ID of field to fill, see cwimp.rpc file
+ *     CharPtr  strP     -- String to fill ID with
+ * Returns:
+ *     FieldPtr          -- Ptr to the field set.
+ */
+FieldPtr SetFieldTextFromStr(Word fieldID, CharPtr strP)
+{
+  VoidHand txtH;
+  
+  txtH = MemHandleNew(StrLen(strP) + 1);
+  if(!txtH) return NULL;
+  
+  StrCopy(MemHandleLock(txtH), strP);
+  
+  // ToDo: SetFieldTextFromHandle should happen *before* unlock
+  MemHandleUnlock(txtH);
+  
+  return SetFieldTextFromHandle(fieldID, txtH);
+}
+
+
+
+/* SetFieldTextFromHandle -- Fills a field with contents of a handle
+ * Args:    
+ *     Word     fieldID  -- ID of field to fill, see cwimp.rpc file
+ *     VoidHand txtH     -- Handle of String to fill ID with
+ * Returns:
+ *     FieldPtr          -- Ptr to the field set.
+ */
+FieldPtr SetFieldTextFromHandle(Word fieldID, VoidHand txtH)
+{
+  Handle   oldTxtH;
+  FormPtr  frm = FrmGetActiveForm();
+  FieldPtr fldP;
+
+  fldP = FrmGetObjectPtr(frm, FrmGetObjectIndex(frm, fieldID));
+  ErrNonFatalDisplayIf(!fldP, "missing field");
+  oldTxtH = FldGetTextHandle(fldP);
+
+  FldSetTextHandle(fldP, (Handle) txtH);
+  // FldEraseField( fldP );
+  FldDrawField(fldP);
+
+  if (oldTxtH)
+    MemHandleFree((VoidHand) oldTxtH);
+
+  return fldP;
+}
+
+
+VoidPtr GetObjectPtr (Word objID) {
+  FormPtr frm;
+  
+  frm = FrmGetActiveForm();
+  
+  return (FrmGetObjectPtr (frm, FrmGetObjectIndex (frm, objID)));
+}
+
+
+
+void ClearFieldText(Word fieldID)
+{
+  SetFieldTextFromHandle(fieldID,NULL);
+}
+
